73f58b3df1f0c2739524caa5d6e994c9cb01d7d4
jhalff <jelmer.halff@yobbers.com>
2023-07-21 14:43:31 +0200

    Small fix for cleaner section-scroller code

--- src/js/section-scroller.js
+++ src/js/section-scroller.js
@@ -1,15 +1,15 @@
-let totalSections = document.querySelectorAll("section").length - 1
+let totalSections = document.querySelectorAll("section").length
 let sectionHeight = screen.height
-let currentSection = 0
+let currentSection = 1
 
 window.addEventListener("wheel", (e) => {
     const scrollDown = e.deltaY > 0 ? true : false
 
     if (scrollDown && currentSection < totalSections) currentSection++
-    else if (!scrollDown && currentSection > 0) currentSection--
+    else if (!scrollDown && currentSection > 1) currentSection--
     
-    let scrollToY = sectionHeight * (currentSection + 1)
-    if (currentSection === 0) scrollToY = 0
+    let scrollToY = sectionHeight * (currentSection)
+    if (currentSection === 1) scrollToY = 0
 
     window.scrollTo({
         top: scrollToY,
