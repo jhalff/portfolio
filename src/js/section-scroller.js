56ccd62c37d9f45d2735373f3bf88f400e3e228e
jhalff <jelmer.halff@yobbers.com>
2023-07-21 15:50:35 +0200

    Fixed section scroller issues, added base for all pages

--- src/js/section-scroller.js
+++ src/js/section-scroller.js
@@ -1,18 +1,26 @@
 let totalSections = document.querySelectorAll("section").length
-let sectionHeight = screen.height
+let sectionHeight = document.querySelector("section").offsetHeight
 let currentSection = 1
+let scrollerActive = false
 
 window.addEventListener("wheel", (e) => {
+    if (scrollerActive) return
+
+    scrollerActive = true
     const scrollDown = e.deltaY > 0 ? true : false
 
     if (scrollDown && currentSection < totalSections) currentSection++
     else if (!scrollDown && currentSection > 1) currentSection--
     
-    let scrollToY = sectionHeight * (currentSection)
+    let scrollToY = sectionHeight * (currentSection - 1)
     if (currentSection === 1) scrollToY = 0
-
+    
     window.scrollTo({
         top: scrollToY,
         behavior: "smooth"
     })
+
+    setTimeout(() => { 
+        scrollerActive = false
+    }, 250)
 })
