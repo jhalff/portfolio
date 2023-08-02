4fd0cc782bf9ae2e7e2e22e794ea991ea39a07b2
jhalff <jelmer.halff@yobbers.com>
2023-08-02 19:26:44 +0200

    Implemented edit project route

--- src/api/routes/r_projects.js
+++ src/api/routes/r_projects.js
@@ -7,7 +7,8 @@ module.exports = function(app) {
         if (req.query.id) {
             db.query(`SELECT * FROM projects WHERE id = ${req.query.id}`, function(err, results) {
                 if (err) throw err
-                res.send(results[0])
+                if (results.length > 0) res.send(results[0])
+                else res.send({ no_results: true })
             })
         }
         else {
@@ -28,18 +29,6 @@ module.exports = function(app) {
         }
     })
 
-    app.get("/cms/projects/new", (req, res) => {
-        if (!req.query.token) { return res.redirect("/") }
-
-        const newProjectQuery = `INSERT INTO projects (image_url, name, tags, description) 
-            VALUES ("/img/placeholder.png", "My Project", "", "")`
-        
-        db.query(newProjectQuery, function(err, result) {
-            if (err) throw err
-            res.redirect(req.get("referer"))
-        }) 
-    })
-
     app.get("/cms/projects/delete", (req, res) => {
         if (!req.query.token) { return res.redirect("/") }
 
@@ -50,17 +39,24 @@ module.exports = function(app) {
     })
 
 
-    // app.post("/cms/projects/edit", (req, res) => {
-    //     let sql
+    app.post("/cms/projects/edit", (req, res) => {
+        let sql = ""
     
-    //     if (req.body.id === "0") {
-    //         sql = `INSERT INTO projects (image_url, name, tags, description) 
-    //             VALUES ('/img/placeholder.png', '${req.body.name}', '${req.body.tags}', '${req.body.description}')`
-    //     }   
+        if (req.body.id === "0") {
+            sql = `INSERT INTO projects (image_url, name, tags, description) 
+                VALUES ('/img/placeholder.png', '${req.body.name}', '${req.body.tags}', '${req.body.description}')`
+        }   
+        else {
+            sql = `UPDATE projects SET 
+                name = '${req.body.name}', 
+                tags = '${req.body.tags}',
+                description = '${req.body.description}'
+                WHERE id = ${req.body.id}`
+        }
     
-    //     // db.query(sql, function(err, result) {
-    //     //     if (err) throw err
-    //     //     res.redirect("/cms?")
-    //     // })
-    // })
+        db.query(sql, function(err, result) {
+            if (err) throw err
+            res.redirect(req.get("referer"))
+        })
+    })
 }
--- src/js/cms-modal-controller.js
+++ src/js/cms-modal-controller.js
@@ -1,10 +1,13 @@
 async function openModal(type, id) {
-    const modal = document.getElementsByClassName(`modal ${type}`)[0]
+    const modal = document.querySelector(`.modal.${type}`)
+    const form = document.querySelector(`.modal.${type} form`)
+
     const response = await fetch(`/cms/${type}?token=${token}&id=${id}`)
     const data = await response.json()
-
     setData(modal, data)
+
     modal.style.display = "block"
+    form.action = `/cms/${type}/edit?token=${token}&id=${id}`
     document.addEventListener("click", modalClickListener)
 }
 
@@ -20,14 +23,19 @@ function modalClickListener(event) {
 function setData(modal, data) {
     const inputs = modal.getElementsByTagName("input")
     const textareas = modal.getElementsByTagName("textarea")
+    const allInputs = []
 
-    for (let i = 0; i < inputs.length; i++) {
-        const property = inputs[i].name
-        if (property in data) { inputs[i].value = data[property] }
-    }
+    for (let i = 0; i < inputs.length; i++) allInputs.push(inputs[i])
+    for (let i = 0; i < textareas.length; i++) allInputs.push(textareas[i])
 
-    for (let i = 0; i < textareas.length; i++) {
-        const property = textareas[i].name
-        if (property in data) { textareas[i].value = data[property] }
+    for (let i = 0; i < allInputs.length; i++) {
+        if (data.no_results) {
+            allInputs[i].value = ""
+            if (allInputs[i].name === "id") allInputs[i].value = 0
+        }
+        else {
+            const property = allInputs[i].name
+            if (property in data) { allInputs[i].value = data[property] }
+        }
     }
 }
