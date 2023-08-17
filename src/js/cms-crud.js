6ebdf66468feca1bbaf124944ae32a71af64278c
jhalff <j_halff@live.nl>
2023-08-17 11:27:32 +0200

    Installed React: Next.js

--- src/js/cms-crud.js
+++
@@ -1,5 +0,0 @@
-function deleteItem(type, id) {
-    const prompt = window.confirm("Are you sure you want to delete this item?")
-    if (prompt) window.location.href = `/cms/${type}/delete?token=${token}&id=${id}`
-    else return
-}
--- src/js/login.js
+++
@@ -1,8 +0,0 @@
-const loginButton = document.querySelector("input[type=submit]")
-const usernameInput = document.querySelector("input[type=text]")
-const passwordInput = document.querySelector("input[type=password")
-
-loginButton.addEventListener("click", () => {
-    sessionStorage.setItem("username", usernameInput.value)
-    sessionStorage.setItem("password", passwordInput.value)
-})
