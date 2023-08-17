ec94e6c7609d36030feeb22819053a1367b9dc11
jhalff <j_halff@live.nl>
2023-08-17 18:18:45 +0200

    Added all old SCSS, site is now 100% converted to React

--- sections/contact.js
+++ sections/contact.js
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function ContactSection() {
     return (
         <>
             <section className="contact">
--- sections/projects.js
+++ sections/projects.js
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function ProjectsSection() {
     return (
         <>
             <section className="projects">
